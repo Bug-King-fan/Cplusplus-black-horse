74ca14e600277ea5c2a4dd2d8e88ef91045ecd99
frank <2363420500@qq.com>
2023-04-01 13:50:12 +0800

    创建 priority_queue-优先队列.cpp

---
+++ 黑马C++/STL/queue容器/priority_queue-优先队列.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <queue> #头文件 
+using namespace std;
+/*
+    top 访问队头元素（堆顶） 
+    empty 队列是否为空
+    size 返回队列内元素个数
+    push 插入元素到队尾 (并排序)
+    emplace 原地构造一个元素并插入队列
+    pop 弹出队头元素
+    swap 交换内容
+*/
+
+/*
+	优先队列 == 堆  ture 
+	定义 大顶堆：priority_queue<int, vector<int>, less<int> > a;
+		 小顶堆：priority_queue<int, vector<int>, greater<int> > a; 
+*/
+int main(){
+	
+	return 0;
+}
